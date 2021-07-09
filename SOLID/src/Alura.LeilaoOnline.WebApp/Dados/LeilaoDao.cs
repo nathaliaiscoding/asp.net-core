b2e75b14141e893de601c1bfd8f78beaafb2a009
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-07-09 16:24:26 -0300

    created LeilaoDao and its new methods to be used on Leilao controllers

---
+++ SOLID/src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,42 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        AppDbContext _context;
+
+        public LeilaoDao(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes() => _context.Leiloes.Include(l => l.Categoria);
+
+        public IEnumerable<Categoria> BuscarCategorias() => _context.Categorias.ToList();
+
+        public Leilao BuscarLeilaoPorId(int id) => _context.Leiloes.Find(id);
+
+
+        public void IncluirLeilao(Leilao obj)
+        {
+            _context.Leiloes.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void EditarLeilao(Leilao obj)
+        {
+            _context.Leiloes.Update(obj);
+            _context.SaveChanges();
+        }
+
+        public void RemoverLeilao(Leilao obj)
+        {
+            _context.Leiloes.Remove(obj);
+            _context.SaveChanges();
+        }
+    }
+}
