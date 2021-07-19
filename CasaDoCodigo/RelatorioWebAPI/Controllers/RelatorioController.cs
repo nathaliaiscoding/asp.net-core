ce09d26c34dc847b48d515d830f24c4892c51001
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-07-19 09:47:38 -0300

    asp.net core parte 4 - aula 1

--- CasaDoCodigo/RelatorioWebAPI/Controllers/RelatorioController.cs
+++ CasaDoCodigo/RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,36 +13,32 @@ namespace RelatorioWebAPI.Controllers
     [ApiController]
     public class RelatorioController : ControllerBase
     {
+
+        private static readonly List<string> Relatorio =
+            new List<string>()
+            {
+                "Primeiro pedido",
+                "Segundo pedido"
+            };
+
         // GET: api/<RelatorioController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in Relatorio)
+            {
+                stringBuilder.AppendLine(item);
+            }
 
-        // GET api/<RelatorioController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
+            return stringBuilder.ToString();
         }
 
         // POST api/<RelatorioController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
-        }
-
-        // PUT api/<RelatorioController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RelatorioController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            Relatorio.Add(value);
         }
     }
 }
